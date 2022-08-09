fc86db807aee207a6316ab45ab81058ad7c9fd66
Mbrace1 <michaelbracey24@gmail.com>
2022-08-09 14:11:29 +0100

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,3 @@
+// Declare variables and assign objects from the DOM
+//register an event listener on the form object.
+//create an async function that create a POST request to the server and handles a response.
